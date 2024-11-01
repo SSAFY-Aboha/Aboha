cc8a8e17320fc4dfc5695f54f4a84f8ed559cf7f
yerim <djadfpa1002@naver.com>
2024-11-01 15:49:30 +0900

    init: 프로젝트 세팅

---
+++ backend/src/test/java/com/ssafy/aboha/AbohaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.aboha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbohaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
