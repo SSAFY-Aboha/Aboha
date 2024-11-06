d61b66b47a2383fabbbc72693793efb48e3c5da4
yerim <djadfpa1002@naver.com>
2024-11-06 12:11:53 +0900

    feat: BaseEntity 생성

---
+++ backend/src/main/java/com/ssafy/aboha/common/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.ssafy.aboha.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+
+    private static final boolean DEFAULT_IS_DELETED = false;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted = DEFAULT_IS_DELETED;
+
+}
