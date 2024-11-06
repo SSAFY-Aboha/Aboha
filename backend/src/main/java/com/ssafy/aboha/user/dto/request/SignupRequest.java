4b066bf4245a65e377d200fbb41c6f04210e42d9
yerim <djadfpa1002@naver.com>
2024-11-06 12:22:08 +0900

    feat: 회원가입 request 생성

---
+++ backend/src/main/java/com/ssafy/aboha/user/dto/request/SignupRequest.java
@@ -0,0 +1,21 @@
+package com.ssafy.aboha.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record SignupRequest(
+        @NotBlank(message = "닉네임이 입력되지 않았습니다.")
+        @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하입니다.")
+        String nickname,
+        @NotBlank(message = "이메일이 입력되지 않았습니다.")
+        @Email(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+[.][0-9A-Za-z]+$",
+                message = "잘못된 이메일 형식입니다.")
+        String email,
+        @NotBlank(message = "비밀번호가 입력되지 않았습니다.")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
+                message = "비밀번호는 8자 아상의 영문, 숫자, 특수문자로 구성되어야 합니다.")
+        String password
+) {
+}
