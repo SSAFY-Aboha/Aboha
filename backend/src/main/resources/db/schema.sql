e7b6db680e47cedaa3f9b5cdba8b930c004acbac
yerim <djadfpa1002@naver.com>
2024-11-06 12:09:22 +0900

    docs: 스키마 변경

--- backend/src/main/resources/db/schema.sql
+++ backend/src/main/resources/db/schema.sql
@@ -12,8 +12,8 @@ USE `aboha`;
 -- Table `aboha`.`sidos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `aboha`.`sidos` (
-                                               `id` INT NOT NULL,
-                                               `name` VARCHAR(20) NULL DEFAULT NULL,
+    `id` BIGINT NOT NULL,
+    `name` VARCHAR(20) NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
     ) ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
@@ -23,9 +23,9 @@ CREATE TABLE IF NOT EXISTS `aboha`.`sidos` (
 -- Table `aboha`.`guguns`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `aboha`.`guguns` (
-                                                `id` INT NOT NULL,
-                                                `sido_id` INT NOT NULL,
-                                                `name` VARCHAR(20) NULL DEFAULT NULL,
+    `id` BIGINT NOT NULL,
+    `sido_id` BIGINT NOT NULL,
+    `name` VARCHAR(20) NULL DEFAULT NULL,
     PRIMARY KEY (`id`),
     INDEX `guguns_sido_to_sidos_id_fk_idx` (`sido_id` ASC),
     CONSTRAINT `guguns_sido_to_sidos_id_fk`
@@ -39,8 +39,8 @@ CREATE TABLE IF NOT EXISTS `aboha`.`guguns` (
 -- Table `aboha`.`contenttypes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `aboha`.`contenttypes` (
-                                                      `id` INT NOT NULL,
-                                                      `name` VARCHAR(45) NULL DEFAULT NULL,
+    `id` BIGINT NOT NULL,
+    `name` VARCHAR(45) NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
     ) ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
@@ -50,19 +50,21 @@ CREATE TABLE IF NOT EXISTS `aboha`.`contenttypes` (
 -- Table `aboha`.`attractions`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `aboha`.`attractions` (
-                                                     `id` INT NOT NULL AUTO_INCREMENT,
-                                                     `content_id` INT NULL DEFAULT NULL,
-                                                     `sido_id` INT NULL,
-                                                     `gugun_id` INT NULL,
-                                                     `content_type_id` INT NULL,
-                                                     `first_image1` VARCHAR(100) NULL DEFAULT NULL,
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `content_id` BIGINT NULL DEFAULT NULL,
+    `sido_id` BIGINT NULL,
+    `gugun_id` BIGINT NULL,
+    `content_type_id` BIGINT NULL,
+    `first_image1` VARCHAR(100) NULL DEFAULT NULL,
     `first_image2` VARCHAR(100) NULL DEFAULT NULL,
-    `map_level` INT NULL DEFAULT NULL,
+    `map_level` BIGINT NULL DEFAULT NULL,
     `latitude` DECIMAL(20,17) NULL DEFAULT NULL,
     `longitude` DECIMAL(20,17) NULL DEFAULT NULL,
     `tel` VARCHAR(20) NULL DEFAULT NULL,
     `addr1` VARCHAR(100) NULL DEFAULT NULL,
     `addr2` VARCHAR(100) NULL DEFAULT NULL,
+    `view_count` BIGINT NOT NULL DEFAULT 0,
+    `like_count` BIGINT NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`),
     INDEX `attractions_contenttype_fk_idx` (`content_type_id` ASC),
     INDEX `attractions_sido_fk_idx` (`sido_id` ASC),
@@ -85,13 +87,14 @@ CREATE TABLE IF NOT EXISTS `aboha`.`attractions` (
 -- Table `aboha`.`users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `aboha`.`users` (
-                                               `id` INT NOT NULL AUTO_INCREMENT,
-                                               `email` VARCHAR(255) NOT NULL,
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `nickname` VARCHAR(50) NOT NULL,
+    `profile_image_url` TEXT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `is_deleted` TINYINT NOT NULL DEFAULT 0,
+    `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `email_UNIQUE` (`email` ASC),
     UNIQUE INDEX `nickname_UNIQUE` (`nickname` ASC)
@@ -100,29 +103,28 @@ CREATE TABLE IF NOT EXISTS `aboha`.`users` (
     COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
--- Table `aboha`.`trip`
+-- Table `aboha`.`reviews`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `aboha`.`trip` (
-                                              `id` INT NOT NULL AUTO_INCREMENT,
-                                              `user_id` INT NOT NULL,
-                                              `attraction_id` INT NOT NULL,
-                                              `title` VARCHAR(255) NOT NULL,
-    `content` TEXT NOT NULL,
-    `view_cnt` INT NOT NULL DEFAULT 0,
-    `like_cnt` INT NOT NULL DEFAULT 0,
+CREATE TABLE IF NOT EXISTS `aboha`.`reviews` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `user_id` BIGINT NULL,
+    `attraction_id` BIGINT NULL,
+    `rating` DECIMAL(3,2) NOT NULL DEFAULT 0.0,
+    `content` VARCHAR(255) NULL DEFAULT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `is_deleted` TINYINT NOT NULL DEFAULT 0,
+    `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`),
-    INDEX `trip_user_fk_idx` (`user_id` ASC),
-    INDEX `trip_attraction_fk_idx` (`attraction_id` ASC),
-    CONSTRAINT `trip_user_fk`
+    INDEX `reviews_user_fk_idx` (`user_id` ASC),
+    INDEX `reviews_attraction_fk_idx` (`attraction_id` ASC),
+    CONSTRAINT `reviews_user_fk`
     FOREIGN KEY (`user_id`)
     REFERENCES `aboha`.`users` (`id`),
-    CONSTRAINT `trip_attraction_fk`
+    CONSTRAINT `reviews_attraction_fk`
     FOREIGN KEY (`attraction_id`)
     REFERENCES `aboha`.`attractions` (`id`)
     ) ENGINE = InnoDB
+    AUTO_INCREMENT = 1
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
 
@@ -130,55 +132,18 @@ CREATE TABLE IF NOT EXISTS `aboha`.`trip` (
 -- Table `aboha`.`likes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `aboha`.`likes` (
-                                               `id` INT NOT NULL AUTO_INCREMENT,
-                                               `user_id` INT NOT NULL,
-                                               `trip_id` INT NOT NULL,
-                                               PRIMARY KEY (`id`),
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `user_id` BIGINT NOT NULL,
+    `attraction_id` BIGINT NOT NULL,
+    PRIMARY KEY (`id`),
     INDEX `likes_user_fk_idx` (`user_id` ASC),
-    INDEX `likes_trip_fk_idx` (`trip_id` ASC),
+    INDEX `likes_attraction_fk_idx` (`attraction_id` ASC),
     CONSTRAINT `likes_user_fk`
     FOREIGN KEY (`user_id`)
     REFERENCES `aboha`.`users` (`id`),
-    CONSTRAINT `likes_trip_fk`
-    FOREIGN KEY (`trip_id`)
-    REFERENCES `aboha`.`trip` (`id`)
-    ) ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
-
--- -----------------------------------------------------
--- Table `aboha`.`comments`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `aboha`.`comments` (
-                                                  `id` INT NOT NULL AUTO_INCREMENT,
-                                                  `user_id` INT NOT NULL,
-                                                  `trip_id` INT NOT NULL,
-                                                  `content` TEXT NOT NULL,
-                                                  PRIMARY KEY (`id`),
-    INDEX `comments_user_fk_idx` (`user_id` ASC),
-    INDEX `comments_trip_fk_idx` (`trip_id` ASC),
-    CONSTRAINT `comments_user_fk`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `aboha`.`users` (`id`),
-    CONSTRAINT `comments_trip_fk`
-    FOREIGN KEY (`trip_id`)
-    REFERENCES `aboha`.`trip` (`id`)
-    ) ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
-
--- -----------------------------------------------------
--- Table `aboha`.`trip_images`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `aboha`.`trip_images` (
-                                                     `id` INT NOT NULL AUTO_INCREMENT,
-                                                     `trip_id` INT NOT NULL,
-                                                     `image_url` VARCHAR(1000) NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `trip_images_trip_fk_idx` (`trip_id` ASC),
-    CONSTRAINT `trip_images_trip_fk`
-    FOREIGN KEY (`trip_id`)
-    REFERENCES `aboha`.`trip` (`id`)
+    CONSTRAINT `likes_attraction_fk`
+    FOREIGN KEY (`attraction_id`)
+    REFERENCES `aboha`.`attractions` (`id`)
     ) ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
