b27aeb08957c891b09a22f742fed46b03dc564fa
yerim <djadfpa1002@naver.com>
2024-11-11 17:11:32 +0900

    feat: Attraction 도메인 생성

---
+++ backend/src/main/java/com/ssafy/aboha/attraction/domain/Attraction.java
@@ -0,0 +1,71 @@
+package com.ssafy.aboha.attraction.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "attractions")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Attraction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "no")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "area_code", referencedColumnName = "sido_code")
+    private Sido sido;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "si_gun_gu_code", referencedColumnName = "gugun_code")
+    private Gugun gugun;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_type_id", referencedColumnName = "content_type_id")
+    private ContentType contentType;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "first_image1")
+    private String firstImage1;
+
+    @Column(name = "first_image2")
+    private String firstImage2;
+
+    @Column(name = "map_level")
+    private Integer mapLevel;
+
+    @Column(name = "latitude", precision = 20, scale = 17)
+    private BigDecimal latitude;
+
+    @Column(name = "longitude", precision = 20, scale = 17)
+    private BigDecimal longitude;
+
+    @Column(name = "tel")
+    private String tel;
+
+    @Column(name = "addr1")
+    private String addr1;
+
+    @Column(name = "addr2")
+    private String addr2;
+
+    @Column(name = "homepage")
+    private String homepage;
+
+    @Column(name = "overview", length = 10000)
+    private String overview;
+
+    @Column(name = "view_count")
+    private Long viewCount;
+
+    @Column(name = "like_count")
+    private Long likeCount;
+}
