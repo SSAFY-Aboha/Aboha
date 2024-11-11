28738e254d0b8bfa25289e80fa240966fe914b8b
yerim <djadfpa1002@naver.com>
2024-11-11 17:09:18 +0900

    docs: 스키마 기존 ssafytrip과 동일하게 수정

--- backend/src/main/resources/db/schema.sql
+++ backend/src/main/resources/db/schema.sql
@@ -1,21 +1,20 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
 -- Schema aboha
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `aboha` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `aboha`;
+CREATE SCHEMA IF NOT EXISTS `aboha` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `aboha` ;
 
 -- -----------------------------------------------------
 -- Table `aboha`.`sidos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `aboha`.`sidos` (
-                                               `id` BIGINT NOT NULL,
-                                               `name` VARCHAR(20) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB
+                                               `no` INT NOT NULL AUTO_INCREMENT,
+                                               `sido_code` INT NULL DEFAULT NULL,
+                                               `sido_name` VARCHAR(20) NULL DEFAULT NULL,
+    PRIMARY KEY (`no`),
+    UNIQUE INDEX `sido_code_UNIQUE` (`sido_code` ASC) VISIBLE)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 18
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
 
@@ -23,15 +22,18 @@ CREATE TABLE IF NOT EXISTS `aboha`.`sidos` (
 -- Table `aboha`.`guguns`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `aboha`.`guguns` (
-                                                `id` BIGINT NOT NULL,
-                                                `sido_id` BIGINT NOT NULL,
-                                                `name` VARCHAR(20) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `guguns_sido_to_sidos_id_fk_idx` (`sido_id` ASC),
-    CONSTRAINT `guguns_sido_to_sidos_id_fk`
-    FOREIGN KEY (`sido_id`)
-    REFERENCES `aboha`.`sidos` (`id`)
-    ) ENGINE = InnoDB
+                                                `no` INT NOT NULL AUTO_INCREMENT,
+                                                `sido_code` INT NULL DEFAULT NULL,
+                                                `gugun_code` INT NULL DEFAULT NULL,
+                                                `gugun_name` VARCHAR(20) NULL DEFAULT NULL,
+    PRIMARY KEY (`no`),
+    INDEX `guguns_sido_to_sidos_cdoe_fk_idx` (`sido_code` ASC) VISIBLE,
+    INDEX `gugun_code_idx` (`gugun_code` ASC) VISIBLE,
+    CONSTRAINT `guguns_sido_to_sidos_cdoe_fk`
+    FOREIGN KEY (`sido_code`)
+    REFERENCES `aboha`.`sidos` (`sido_code`))
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 235
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
 
@@ -39,10 +41,10 @@ CREATE TABLE IF NOT EXISTS `aboha`.`guguns` (
 -- Table `aboha`.`contenttypes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `aboha`.`contenttypes` (
-                                                      `id` BIGINT NOT NULL,
-                                                      `name` VARCHAR(45) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB
+                                                      `content_type_id` INT NOT NULL,
+                                                      `content_type_name` VARCHAR(45) NULL DEFAULT NULL,
+    PRIMARY KEY (`content_type_id`))
+    ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
 
@@ -50,35 +52,39 @@ CREATE TABLE IF NOT EXISTS `aboha`.`contenttypes` (
 -- Table `aboha`.`attractions`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `aboha`.`attractions` (
-                                                     `id` BIGINT NOT NULL AUTO_INCREMENT,
-                                                     `sido_id` BIGINT NULL,
-                                                     `gugun_id` BIGINT NULL,
-                                                     `content_type_id` BIGINT NULL,
-                                                     `first_image1` VARCHAR(100) NULL DEFAULT NULL,
+                                                     `no` INT NOT NULL AUTO_INCREMENT,
+                                                     `content_id` INT NULL DEFAULT NULL,
+                                                     `title` VARCHAR(500) NULL DEFAULT NULL,
+    `content_type_id` INT NULL DEFAULT NULL,
+    `area_code` INT NULL DEFAULT NULL,
+    `si_gun_gu_code` INT NULL DEFAULT NULL,
+    `first_image1` VARCHAR(100) NULL DEFAULT NULL,
     `first_image2` VARCHAR(100) NULL DEFAULT NULL,
-    `map_level` BIGINT NULL DEFAULT NULL,
+    `map_level` INT NULL DEFAULT NULL,
     `latitude` DECIMAL(20,17) NULL DEFAULT NULL,
     `longitude` DECIMAL(20,17) NULL DEFAULT NULL,
     `tel` VARCHAR(20) NULL DEFAULT NULL,
     `addr1` VARCHAR(100) NULL DEFAULT NULL,
     `addr2` VARCHAR(100) NULL DEFAULT NULL,
+    `homepage` VARCHAR(1000) NULL DEFAULT NULL,
+    `overview` VARCHAR(10000) NULL DEFAULT NULL,
     `view_count` BIGINT NOT NULL DEFAULT 0,
     `like_count` BIGINT NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`),
-    INDEX `attractions_contenttype_fk_idx` (`content_type_id` ASC),
-    INDEX `attractions_sido_fk_idx` (`sido_id` ASC),
-    INDEX `attractions_gugun_fk_idx` (`gugun_id` ASC),
-    CONSTRAINT `attractions_sido_fk`
-    FOREIGN KEY (`sido_id`)
-    REFERENCES `aboha`.`sidos` (`id`),
-    CONSTRAINT `attractions_gugun_fk`
-    FOREIGN KEY (`gugun_id`)
-    REFERENCES `aboha`.`guguns` (`id`),
-    CONSTRAINT `attractions_contenttype_fk`
+    PRIMARY KEY (`no`),
+    INDEX `attractions_typeid_to_types_typeid_fk_idx` (`content_type_id` ASC) VISIBLE,
+    INDEX `attractions_sido_to_sidos_code_fk_idx` (`area_code` ASC) VISIBLE,
+    INDEX `attractions_sigungu_to_guguns_gugun_fk_idx` (`si_gun_gu_code` ASC) VISIBLE,
+    CONSTRAINT `attractions_area_to_sidos_code_fk`
+    FOREIGN KEY (`area_code`)
+    REFERENCES `aboha`.`sidos` (`sido_code`),
+    CONSTRAINT `attractions_sigungu_to_guguns_gugun_fk`
+    FOREIGN KEY (`si_gun_gu_code`)
+    REFERENCES `aboha`.`guguns` (`gugun_code`),
+    CONSTRAINT `attractions_typeid_to_types_typeid_fk`
     FOREIGN KEY (`content_type_id`)
-    REFERENCES `aboha`.`contenttypes` (`id`)
-    ) ENGINE = InnoDB
-    AUTO_INCREMENT = 1
+    REFERENCES `aboha`.`contenttypes` (`content_type_id`))
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 56650
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
 
@@ -107,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `aboha`.`users` (
 CREATE TABLE IF NOT EXISTS `aboha`.`reviews` (
                                                  `id` BIGINT NOT NULL AUTO_INCREMENT,
                                                  `user_id` BIGINT NULL,
-                                                 `attraction_id` BIGINT NULL,
+                                                 `attraction_id` INT NULL,
                                                  `rating` DECIMAL(3,2) NOT NULL DEFAULT 0.0,
     `content` VARCHAR(255) NULL DEFAULT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -121,9 +127,8 @@ CREATE TABLE IF NOT EXISTS `aboha`.`reviews` (
     REFERENCES `aboha`.`users` (`id`),
     CONSTRAINT `reviews_attraction_fk`
     FOREIGN KEY (`attraction_id`)
-    REFERENCES `aboha`.`attractions` (`id`)
+    REFERENCES `aboha`.`attractions` (`no`)
     ) ENGINE = InnoDB
-    AUTO_INCREMENT = 1
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
 
@@ -133,7 +138,7 @@ CREATE TABLE IF NOT EXISTS `aboha`.`reviews` (
 CREATE TABLE IF NOT EXISTS `aboha`.`likes` (
                                                `id` BIGINT NOT NULL AUTO_INCREMENT,
                                                `user_id` BIGINT NOT NULL,
-                                               `attraction_id` BIGINT NOT NULL,
+                                               `attraction_id` INT NOT NULL,
                                                PRIMARY KEY (`id`),
     INDEX `likes_user_fk_idx` (`user_id` ASC),
     INDEX `likes_attraction_fk_idx` (`attraction_id` ASC),
@@ -142,11 +147,7 @@ CREATE TABLE IF NOT EXISTS `aboha`.`likes` (
     REFERENCES `aboha`.`users` (`id`),
     CONSTRAINT `likes_attraction_fk`
     FOREIGN KEY (`attraction_id`)
-    REFERENCES `aboha`.`attractions` (`id`)
+    REFERENCES `aboha`.`attractions` (`no`)
     ) ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
