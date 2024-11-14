784e3e01cf949707724c4cd874029315cb4fdc05
yerim <djadfpa1002@naver.com>
2024-11-14 16:54:48 +0900

    feat: Attraction 목록 조회 및 필터링 관련 request 생성

---
+++ backend/src/main/java/com/ssafy/aboha/attraction/dto/request/AttractionSearchRequest.java
@@ -0,0 +1,13 @@
+package com.ssafy.aboha.attraction.dto.request;
+
+import jakarta.annotation.Nullable;
+
+public record AttractionSearchRequest(
+        @Nullable
+        Integer sidoCode,
+        @Nullable
+        Integer gugunCode,
+        @Nullable
+        Integer contentTypeId
+) {
+}
