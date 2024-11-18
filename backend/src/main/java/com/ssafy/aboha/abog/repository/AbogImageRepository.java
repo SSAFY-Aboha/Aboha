383bc4f928bc21c57b853cccb41fba607e309505
EomYerim <djadpfa1002@naver.com>
2024-11-19 06:35:52 +0900

    feat: Abog 이미지 repository 생성

---
+++ backend/src/main/java/com/ssafy/aboha/abog/repository/AbogImageRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.aboha.abog.repository;
+
+import com.ssafy.aboha.abog.domain.AbogImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AbogImageRepository extends JpaRepository<AbogImage, Integer> {
+
+}
