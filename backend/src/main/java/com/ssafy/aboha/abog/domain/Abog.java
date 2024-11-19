911f787f8fe903606723c090ce197419b14389ae
EomYerim <djadpfa1002@naver.com>
2024-11-20 04:17:27 +0900

    refactor: Abog 도메인 Image 양방향에서 단방향 맵핑으로 변경

--- backend/src/main/java/com/ssafy/aboha/abog/domain/Abog.java
+++ backend/src/main/java/com/ssafy/aboha/abog/domain/Abog.java
@@ -4,7 +4,6 @@ package com.ssafy.aboha.abog.domain;
 import com.ssafy.aboha.attraction.domain.Attraction;
 import com.ssafy.aboha.common.BaseEntity;
 import com.ssafy.aboha.user.domain.User;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -13,9 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -50,9 +47,6 @@ public class Abog extends BaseEntity {
 
     @Column(name = "comment_count")
     private Long commentCount;
-
-    @OneToMany(mappedBy = "abog", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<AbogImage> images;
 
     // TODO: 해시태그 적용 전
 
