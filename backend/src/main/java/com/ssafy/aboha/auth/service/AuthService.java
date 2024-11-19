02e92687613b717276e7337f081aec55f2d2fd3e
EomYerim <djadpfa1002@naver.com>
2024-11-20 04:13:56 +0900

    rename: LoginRequest request 폴더로 이동으로 인한 import 변경

--- backend/src/main/java/com/ssafy/aboha/auth/service/AuthService.java
+++ backend/src/main/java/com/ssafy/aboha/auth/service/AuthService.java
@@ -1,15 +1,14 @@
 package com.ssafy.aboha.auth.service;
 
-import com.ssafy.aboha.auth.dto.LoginRequest;
+import com.ssafy.aboha.auth.dto.request.LoginRequest;
 import com.ssafy.aboha.common.exception.UnauthorizedException;
 import com.ssafy.aboha.user.domain.User;
 import com.ssafy.aboha.user.dto.response.UserResponse;
 import com.ssafy.aboha.user.repository.UserRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
