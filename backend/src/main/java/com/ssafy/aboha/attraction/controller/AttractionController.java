5c8af50a8de8986ee1d29f17b06492ba34f2fbfa
yerim <djadfpa1002@naver.com>
2024-11-20 14:23:55 +0900

    feat: Attraction 좋아요 토글 controller 생성

--- backend/src/main/java/com/ssafy/aboha/attraction/controller/AttractionController.java
+++ backend/src/main/java/com/ssafy/aboha/attraction/controller/AttractionController.java
@@ -7,6 +7,11 @@ import com.ssafy.aboha.attraction.dto.response.AttractionResponse;
 import com.ssafy.aboha.attraction.dto.response.AttractionSummary;
 import com.ssafy.aboha.attraction.service.AttractionService;
 import com.ssafy.aboha.common.dto.response.PaginatedResponse;
+import com.ssafy.aboha.common.exception.UnauthorizedException;
+import com.ssafy.aboha.like.dto.LikeResponse;
+import com.ssafy.aboha.like.service.LikeService;
+import com.ssafy.aboha.user.dto.response.UserResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +27,7 @@ import java.util.List;
 public class AttractionController {
 
     private final AttractionService attractionService;
+    private final LikeService likeService;
 
     // 관광지 목록 필터링 조회
     @GetMapping
@@ -43,6 +49,22 @@ public class AttractionController {
     @GetMapping("/search")
     public ResponseEntity<List<AttractionSummary>> searchAttractions(@Valid @ModelAttribute AttractionSearchRequest request) {
         List<AttractionSummary> response = attractionService.getAttractionsByTitle(request.keyword().trim());
+        return ResponseEntity.ok().body(response);
+    }
+
+    // 관광지 좋아요
+    @PostMapping("/{id}/like")
+    public ResponseEntity<LikeResponse> toggleAttractionLike(
+            @PathVariable("id") Integer attractionId,
+            HttpSession session
+    ) {
+        // 세션에서 인증된 사용자 정보 확인
+        UserResponse userResponse = (UserResponse) session.getAttribute("user");
+        if (userResponse == null) {
+            throw new UnauthorizedException("로그인이 필요합니다."); // 인증 실패
+        }
+
+        LikeResponse response = likeService.toggleLike(userResponse.id(), attractionId);
         return ResponseEntity.ok().body(response);
     }
 }
