80a10294b4c4655cdc92b5bfc69f083ffae2dc20
yerim <djadfpa1002@naver.com>
2024-11-20 14:52:27 +0900

    rename: Attraction 좋아요 service 이름 변경

--- backend/src/main/java/com/ssafy/aboha/attraction/controller/AttractionController.java
+++ backend/src/main/java/com/ssafy/aboha/attraction/controller/AttractionController.java
@@ -64,7 +64,7 @@ public class AttractionController {
             throw new UnauthorizedException("로그인이 필요합니다."); // 인증 실패
         }
 
-        LikeResponse response = likeService.toggleLike(userResponse.id(), attractionId);
+        LikeResponse response = likeService.toggleAttractionLike(userResponse.id(), attractionId);
         return ResponseEntity.ok().body(response);
     }
 }
