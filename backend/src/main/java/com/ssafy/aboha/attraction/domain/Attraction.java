9198397d50665bb786f3ab14bed43a444b4354f9
yerim <djadfpa1002@naver.com>
2024-11-20 14:22:09 +0900

    feat: Attraction 도메인에 좋아요 증가/감소 로직 추가

--- backend/src/main/java/com/ssafy/aboha/attraction/domain/Attraction.java
+++ backend/src/main/java/com/ssafy/aboha/attraction/domain/Attraction.java
@@ -1,18 +1,11 @@
 package com.ssafy.aboha.attraction.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import java.math.BigDecimal;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "attractions")
@@ -84,6 +77,14 @@ public class Attraction {
 
     public void increaseViewCount() {
         this.viewCount++;
+    }
+
+    public void increaseLikeCount() {
+        this.likeCount++;
+    }
+
+    public void decreaseLikeCount() {
+        this.likeCount--;
     }
 
 }
