2bb982f490d9eed0817143aed28f7207312186ff
yerim <djadfpa1002@naver.com>
2024-11-20 11:39:03 +0900

    feat: Attraction 관광지 이름 검색 controller 추가

--- backend/src/main/java/com/ssafy/aboha/attraction/controller/AttractionController.java
+++ backend/src/main/java/com/ssafy/aboha/attraction/controller/AttractionController.java
@@ -1,15 +1,20 @@
 package com.ssafy.aboha.attraction.controller;
 
+import com.ssafy.aboha.attraction.dto.request.AttractionReadRequest;
 import com.ssafy.aboha.attraction.dto.request.AttractionSearchRequest;
 import com.ssafy.aboha.attraction.dto.response.AttractionInfo;
 import com.ssafy.aboha.attraction.dto.response.AttractionResponse;
+import com.ssafy.aboha.attraction.dto.response.AttractionSummary;
 import com.ssafy.aboha.attraction.service.AttractionService;
 import com.ssafy.aboha.common.dto.response.PaginatedResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/attractions")
@@ -21,7 +26,7 @@ public class AttractionController {
     // 관광지 목록 필터링 조회
     @GetMapping
     public ResponseEntity<PaginatedResponse<AttractionInfo>> filterAttractions(
-            @ModelAttribute AttractionSearchRequest request,
+            @ModelAttribute AttractionReadRequest request,
             @PageableDefault(size = 12) Pageable pageable) {
         PaginatedResponse<AttractionInfo> response = attractionService.getAttractionsByFilters(request, pageable);
         return ResponseEntity.ok().body(response);
@@ -29,8 +34,15 @@ public class AttractionController {
 
     // 관광지 상세 조회
     @GetMapping("/{id}")
-    public ResponseEntity<AttractionResponse> getAttractionById(@PathVariable Integer id) {
+    public ResponseEntity<AttractionResponse> getAttractionById(@PathVariable("id") Integer id) {
         AttractionResponse response = attractionService.getAttraction(id);
+        return ResponseEntity.ok().body(response);
+    }
+
+    // 아보그 생성 시 관광지 이름 검색
+    @GetMapping("/search")
+    public ResponseEntity<List<AttractionSummary>> searchAttractions(@Valid @ModelAttribute AttractionSearchRequest request) {
+        List<AttractionSummary> response = attractionService.getAttractionsByTitle(request.keyword().trim());
         return ResponseEntity.ok().body(response);
     }
 }
