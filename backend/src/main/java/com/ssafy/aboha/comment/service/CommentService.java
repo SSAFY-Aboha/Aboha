7362feef00e228184fef549423afd42c52800811
yerim <djadfpa1002@naver.com>
2024-11-21 12:15:14 +0900

    feat: Comment 목록 및 상세 조회 service 추가

--- backend/src/main/java/com/ssafy/aboha/comment/service/CommentService.java
+++ backend/src/main/java/com/ssafy/aboha/comment/service/CommentService.java
@@ -4,6 +4,7 @@ import com.ssafy.aboha.abog.domain.Abog;
 import com.ssafy.aboha.abog.repository.AbogRepository;
 import com.ssafy.aboha.comment.domain.Comment;
 import com.ssafy.aboha.comment.dto.request.CommentRequest;
+import com.ssafy.aboha.comment.dto.response.CommentResponse;
 import com.ssafy.aboha.comment.repository.CommentRepository;
 import com.ssafy.aboha.common.exception.NotFoundException;
 import com.ssafy.aboha.user.domain.User;
@@ -11,6 +12,8 @@ import com.ssafy.aboha.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -30,9 +33,9 @@ public class CommentService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new NotFoundException("로그인한 사용자가 존재하지 않습니다."));
 
-        // 관광지 확인
+        // 아보그 확인
         Abog abog = abogRepository.findByAbogId(request.abogId())
-                .orElseThrow(() -> new NotFoundException("해당 아보그가 존재하지 않습니다."));
+                .orElseThrow(() -> new NotFoundException("아보그가 존재하지 않습니다."));
 
         // 댓글 생성
         Comment comment = Comment.builder()
@@ -44,5 +47,29 @@ public class CommentService {
         commentRepository.save(comment);
 
         return comment.getId();
+    }
+
+    /**
+     * 댓글 목록 조회
+     */
+    public List<CommentResponse> getComments(Integer abogId) {
+        Abog abog = abogRepository.findByAbogId(abogId)
+                .orElseThrow(() -> new NotFoundException("아보그가 존재하지 않습니다."));
+
+        List<Comment> comments = commentRepository.findByAbog(abog);
+        return comments.stream()
+                .map(CommentResponse::from)
+                .toList();
+    }
+
+    /**
+     * 댓글 상세 조회
+     */
+    public CommentResponse getComment(Integer id) {
+        // 댓글 확인
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("댓글이 존재하지 않습니다."));
+
+        return CommentResponse.from(comment);
     }
 }
