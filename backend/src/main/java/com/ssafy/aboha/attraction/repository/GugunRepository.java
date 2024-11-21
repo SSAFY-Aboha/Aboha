ff02bbdfc32be0e6dad36d860e5e988389d4f153
yerim <djadfpa1002@naver.com>
2024-11-21 16:01:36 +0900

    feat: Gugun 시도 코드 기반 구군 조회 repository 추가

--- backend/src/main/java/com/ssafy/aboha/attraction/repository/GugunRepository.java
+++ backend/src/main/java/com/ssafy/aboha/attraction/repository/GugunRepository.java
@@ -2,6 +2,8 @@ package com.ssafy.aboha.attraction.repository;
 
 import com.ssafy.aboha.attraction.domain.Gugun;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +13,8 @@ public interface GugunRepository extends JpaRepository<Gugun, Integer> {
     Optional<Gugun> findByCodeAndSido_Code(Integer code, Integer sidoCode);
 
     List<Gugun> findBySidoCode(Integer sidoCode);
+
+    @Query("SELECT g FROM Gugun g WHERE g.sido.code = :sidoCode")
+    List<Gugun> findGugunsBySido(@Param("sidoCode") Integer sidoCode);
 
 }
