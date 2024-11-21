8c0c5d0c66e6fdf53214d1cccee807095887650c
yerim <djadfpa1002@naver.com>
2024-11-21 12:14:58 +0900

    feat: Comment 목록 조회 repository 추가

--- backend/src/main/java/com/ssafy/aboha/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/ssafy/aboha/comment/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package com.ssafy.aboha.comment.repository;
 
+import com.ssafy.aboha.abog.domain.Abog;
 import com.ssafy.aboha.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    List<Comment> findByAbog(Abog abog);
 }
