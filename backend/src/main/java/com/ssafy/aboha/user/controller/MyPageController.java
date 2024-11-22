37119225945fff16affb9b1ab34925fa4b5990ba
yerim <djadfpa1002@naver.com>
2024-11-22 12:12:19 +0900

    feat: Attraction 사용자가 리뷰 남긴 목록 조회 controller 추가

--- backend/src/main/java/com/ssafy/aboha/user/controller/MyPageController.java
+++ backend/src/main/java/com/ssafy/aboha/user/controller/MyPageController.java
@@ -1,6 +1,7 @@
 package com.ssafy.aboha.user.controller;
 
 import com.ssafy.aboha.attraction.dto.response.MyLikedAttractionResponse;
+import com.ssafy.aboha.attraction.dto.response.MyReviewedAttractionResponse;
 import com.ssafy.aboha.attraction.service.AttractionService;
 import com.ssafy.aboha.common.dto.response.KeySetPaginatedResponse;
 import com.ssafy.aboha.common.exception.UnauthorizedException;
@@ -34,6 +35,22 @@ public class MyPageController {
         }
 
         KeySetPaginatedResponse<MyLikedAttractionResponse> response = attractionService.getUserLikedAttractions(userResponse.id(), pageable);
+        return ResponseEntity.ok().body(response);
+    }
+
+    // 사용자가 남긴 리뷰 목록 조회
+    @GetMapping("/reviews")
+    public ResponseEntity<KeySetPaginatedResponse<MyReviewedAttractionResponse>> getUserReviewedAttractions(
+            HttpSession session,
+            @PageableDefault(size = 12) Pageable pageable
+    ) {
+        // 세션에서 인증된 사용자 정보 확인
+        UserResponse userResponse = (UserResponse) session.getAttribute("user");
+        if (userResponse == null) {
+            throw new UnauthorizedException("로그인이 필요합니다."); // 인증 실패
+        }
+
+        KeySetPaginatedResponse<MyReviewedAttractionResponse> response = attractionService.getUserReviewedAttractions(userResponse.id(), pageable);
         return ResponseEntity.ok().body(response);
     }
 }
