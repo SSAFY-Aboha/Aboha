f981baa7b5ec075def05b5b451a152f9c1ea6b90
yerim <djadfpa1002@naver.com>
2024-11-22 11:17:37 +0900

    feat: User repository id 확인 추가

--- backend/src/main/java/com/ssafy/aboha/user/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/aboha/user/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.ssafy.aboha.user.repository;
 
 import com.ssafy.aboha.user.domain.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
@@ -11,5 +12,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsUserByEmail(String email);
 
     Optional<User> findUserByEmail(String email);
+
+    boolean existsById(int id);
 
 }
