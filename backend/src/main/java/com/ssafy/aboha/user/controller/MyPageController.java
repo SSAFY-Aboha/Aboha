3522f2bd8c59b58b055c915d6e188ff20414ec50
yerim <djadfpa1002@naver.com>
2024-11-22 11:18:36 +0900

    feat: Attraction 사용자가 좋아요한 목록 조회 controller 생성

---
+++ backend/src/main/java/com/ssafy/aboha/user/controller/MyPageController.java
@@ -0,0 +1,39 @@
+package com.ssafy.aboha.user.controller;
+
+import com.ssafy.aboha.attraction.dto.response.AttractionInfo;
+import com.ssafy.aboha.attraction.service.AttractionService;
+import com.ssafy.aboha.common.dto.response.KeySetPaginatedResponse;
+import com.ssafy.aboha.common.exception.UnauthorizedException;
+import com.ssafy.aboha.user.dto.response.UserResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class MyPageController {
+
+    private final AttractionService attractionService;
+
+    // 사용자가 좋아요한 관광지 목록 조회
+    @GetMapping("/likes")
+    public ResponseEntity<KeySetPaginatedResponse<AttractionInfo>> getUserLikedAttractions(
+            HttpSession session,
+            @PageableDefault(size = 12) Pageable pageable
+    ) {
+        // 세션에서 인증된 사용자 정보 확인
+        UserResponse userResponse = (UserResponse) session.getAttribute("user");
+        if (userResponse == null) {
+            throw new UnauthorizedException("로그인이 필요합니다."); // 인증 실패
+        }
+
+        KeySetPaginatedResponse<AttractionInfo> response = attractionService.getUserLikedAttractions(userResponse.id(), pageable);
+        return ResponseEntity.ok().body(response);
+    }
+}
