662ccb60a49d921b040cab15f726a3a6b97f99f4
EomYerim <djadpfa1002@naver.com>
2024-11-23 21:46:02 +0900

    feat: Like 사용자가 관광지 좋아요 눌렀는지 확인 repository 추가

--- backend/src/main/java/com/ssafy/aboha/like/repository/AttractionLikeRepository.java
+++ backend/src/main/java/com/ssafy/aboha/like/repository/AttractionLikeRepository.java
@@ -3,11 +3,13 @@ package com.ssafy.aboha.like.repository;
 import com.ssafy.aboha.attraction.domain.Attraction;
 import com.ssafy.aboha.like.domain.AttractionLike;
 import com.ssafy.aboha.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttractionLikeRepository extends JpaRepository<AttractionLike, Integer> {
 
     Optional<AttractionLike> findByUserAndAttraction(User user, Attraction attraction);
+
+    boolean existsByUserIdAndAttractionId(Integer user_id, Integer attraction_id);
+
 }
