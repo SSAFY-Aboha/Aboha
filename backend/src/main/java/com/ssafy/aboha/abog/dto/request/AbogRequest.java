c94e32d48154b2b61b9f283af326ab73681cbf3d
EomYerim <djadpfa1002@naver.com>
2024-11-23 14:31:27 +0900

    refactor: Abog 생성 시 request 속성 프론트와 통일을 위한 수정

--- backend/src/main/java/com/ssafy/aboha/abog/dto/request/AbogRequest.java
+++ backend/src/main/java/com/ssafy/aboha/abog/dto/request/AbogRequest.java
@@ -4,19 +4,18 @@ package com.ssafy.aboha.abog.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 public record AbogRequest(
-        @NotNull(message = "관광지 식별자가 입력되지 않았습니다.")
-        Integer attractionId,
-        @NotBlank(message = "제목이 입력되지 않았습니다.")
-        String title,
-        @NotBlank(message = "내용이 입력되지 않았습니다.")
-        String content,
-        @NotNull(message = "최소 1개의 이미지를 업로드해야 합니다.")
-        @Size(min = 1, message = "최소 1개의 이미지를 업로드해야 합니다.")
-        List<MultipartFile> images
+    @NotNull(message = "관광지 식별자가 입력되지 않았습니다.")
+    Integer attraction,
+    @NotBlank(message = "제목이 입력되지 않았습니다.")
+    String title,
+    @NotBlank(message = "내용이 입력되지 않았습니다.")
+    String content,
+    @NotNull(message = "최소 1개의 이미지를 업로드해야 합니다.")
+    @Size(min = 1, message = "최소 1개의 이미지를 업로드해야 합니다.")
+    List<MultipartFile> imageFiles
 ) {
 }
