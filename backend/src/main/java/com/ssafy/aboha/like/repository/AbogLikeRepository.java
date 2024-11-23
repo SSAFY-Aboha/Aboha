8760d2b0cb5d0f30d3bb52ef11f69210c7d9d540
EomYerim <djadpfa1002@naver.com>
2024-11-24 00:24:04 +0900

    feat: Like 사용자 아보그 눌렀는지 확인 repository 추가

--- backend/src/main/java/com/ssafy/aboha/like/repository/AbogLikeRepository.java
+++ backend/src/main/java/com/ssafy/aboha/like/repository/AbogLikeRepository.java
@@ -3,11 +3,19 @@ package com.ssafy.aboha.like.repository;
 import com.ssafy.aboha.abog.domain.Abog;
 import com.ssafy.aboha.like.domain.AbogLike;
 import com.ssafy.aboha.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AbogLikeRepository extends JpaRepository<AbogLike, Integer> {
 
     Optional<AbogLike> findByUserAndAbog(User user, Abog abog);
+
+    boolean existsByUserIdAndAbogId(Integer userId, Integer abogId);
+
+    // 로그인한 사용자가 좋아요한 Abog ID 목록 반환
+    @Query("SELECT al.abog.id FROM AbogLike al WHERE al.user.id = :userId")
+    List<Integer> findAbogIdByUserId(Integer userId);
+
 }
