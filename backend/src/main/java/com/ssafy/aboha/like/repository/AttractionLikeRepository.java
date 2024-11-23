663b6b6b2ac2291d936610c063ebb527463a814d
EomYerim <djadpfa1002@naver.com>
2024-11-23 23:45:39 +0900

    feat: Like 사용자 ID로 관광지 ID 찾기 repository 추가

--- backend/src/main/java/com/ssafy/aboha/like/repository/AttractionLikeRepository.java
+++ backend/src/main/java/com/ssafy/aboha/like/repository/AttractionLikeRepository.java
@@ -3,13 +3,18 @@ package com.ssafy.aboha.like.repository;
 import com.ssafy.aboha.attraction.domain.Attraction;
 import com.ssafy.aboha.like.domain.AttractionLike;
 import com.ssafy.aboha.user.domain.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AttractionLikeRepository extends JpaRepository<AttractionLike, Integer> {
 
     Optional<AttractionLike> findByUserAndAttraction(User user, Attraction attraction);
 
     boolean existsByUserIdAndAttractionId(Integer user_id, Integer attraction_id);
+
+    @Query("SELECT al.attraction.id FROM AttractionLike al WHERE al.user.id = :userId")
+    List<Integer> findAttractionIdByUserId(Integer userId);
 
 }
