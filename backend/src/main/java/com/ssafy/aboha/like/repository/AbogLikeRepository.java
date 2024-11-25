e34c6f83b6943c92f5da0ad0689899cf63d71510
yerim <djadfpa1002@naver.com>
2024-11-25 09:30:59 +0900

    fix: Like repository @Param 누락 해결

--- backend/src/main/java/com/ssafy/aboha/like/repository/AbogLikeRepository.java
+++ backend/src/main/java/com/ssafy/aboha/like/repository/AbogLikeRepository.java
@@ -3,10 +3,12 @@ package com.ssafy.aboha.like.repository;
 import com.ssafy.aboha.abog.domain.Abog;
 import com.ssafy.aboha.like.domain.AbogLike;
 import com.ssafy.aboha.user.domain.User;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface AbogLikeRepository extends JpaRepository<AbogLike, Integer> {
 
@@ -16,6 +18,6 @@ public interface AbogLikeRepository extends JpaRepository<AbogLike, Integer> {
 
     // 로그인한 사용자가 좋아요한 Abog ID 목록 반환
     @Query("SELECT al.abog.id FROM AbogLike al WHERE al.user.id = :userId")
-    List<Integer> findAbogIdByUserId(Integer userId);
+    List<Integer> findAbogIdByUserId(@Param("userId") Integer userId);
 
 }
