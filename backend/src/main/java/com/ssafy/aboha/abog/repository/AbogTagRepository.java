7d76e6abd55c28c8bd7abf821a6899a148bfc0c0
yerim <djadfpa1002@naver.com>
2024-11-25 12:38:24 +0900

    feat: Abog 태그 repository 생성

---
+++ backend/src/main/java/com/ssafy/aboha/abog/repository/AbogTagRepository.java
@@ -0,0 +1,12 @@
+package com.ssafy.aboha.abog.repository;
+
+import com.ssafy.aboha.abog.domain.Abog;
+import com.ssafy.aboha.abog.domain.AbogTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AbogTagRepository extends JpaRepository<AbogTag, Integer> {
+
+    List<AbogTag> findByAbog(Abog abog);
+}
