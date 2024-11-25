b576ce00c1f8ba235af6c1a8952a93955dfbc797
EomYerim <djadpfa1002@naver.com>
2024-11-26 06:19:38 +0900

    fix: Abog 생성 request에 tags null 관련 validation 추가

--- backend/src/main/java/com/ssafy/aboha/abog/dto/request/AbogRequest.java
+++ backend/src/main/java/com/ssafy/aboha/abog/dto/request/AbogRequest.java
@@ -4,9 +4,8 @@ package com.ssafy.aboha.abog.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 public record AbogRequest(
         @NotNull(message = "관광지 식별자가 입력되지 않았습니다.")
@@ -18,6 +17,7 @@ public record AbogRequest(
         @NotNull(message = "최소 1개의 이미지를 업로드해야 합니다.")
         @Size(min = 1, message = "최소 1개의 이미지를 업로드해야 합니다.")
         List<MultipartFile> imageFiles,
+        @NotNull(message = "최소 1개의 태그를 입력해야 합니다.")
         @Size(min = 1, max = 3, message = "태그는 최소 1개부터 최대 3개까지 가능합니다.")
         List<String> tags
 ) {
