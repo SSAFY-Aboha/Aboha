8bb70b08076358f378acd6b2a25b6ae2529230dd
yerim <djadfpa1002@naver.com>
2024-11-25 09:53:09 +0900

    feat: User @SQLRestriction 추가

--- backend/src/main/java/com/ssafy/aboha/user/domain/User.java
+++ backend/src/main/java/com/ssafy/aboha/user/domain/User.java
@@ -1,21 +1,18 @@
 package com.ssafy.aboha.user.domain;
 
 import com.ssafy.aboha.common.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@SQLRestriction("is_deleted = 0")
 public class User extends BaseEntity {
 
     @Id
