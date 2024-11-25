1ea56f028049e954fb646da997ac08851f06d87d
yerim <djadfpa1002@naver.com>
2024-11-25 15:11:46 +0900

    fix: 이미지 url 앞에 context path 붙이도록 수정

--- backend/src/main/java/com/ssafy/aboha/user/dto/response/UserSummary.java
+++ backend/src/main/java/com/ssafy/aboha/user/dto/response/UserSummary.java
@@ -1,5 +1,6 @@
 package com.ssafy.aboha.user.dto.response;
 
+import com.ssafy.aboha.common.utils.ServletUtils;
 import com.ssafy.aboha.user.domain.User;
 import lombok.Builder;
 
@@ -9,12 +10,17 @@ public record UserSummary(
     String nickname,
     String profileImageUrl
 ) {
+    private static final String CONTEXT_PATH = ServletUtils.getContextPath();
 
     public static UserSummary from(User user) {
+        String profileImageUrl = user.getProfileImageUrl() != null
+                ? CONTEXT_PATH + user.getProfileImageUrl()
+                :null;
+
         return UserSummary.builder()
             .id(user.getId())
             .nickname(user.getNickname())
-            .profileImageUrl(user.getProfileImageUrl())
+            .profileImageUrl(profileImageUrl)
             .build();
     }
 
