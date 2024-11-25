1aadc00ac5ea6ad5b6502f89e2ff9eccfe2940bc
yerim <djadfpa1002@naver.com>
2024-11-25 17:28:54 +0900

    feat: Abog @SQLRestriction 적용해 soft delete 반영

--- backend/src/main/java/com/ssafy/aboha/abog/domain/Abog.java
+++ backend/src/main/java/com/ssafy/aboha/abog/domain/Abog.java
@@ -9,11 +9,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "abogs")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@SQLRestriction("is_deleted = 0")
 public class Abog extends BaseEntity {
 
     @Id
